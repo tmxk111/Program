4ceff4354b6cf6df27dcd0533d764b95af8d1c0c
tmxk111 <jiwenyang666@163.com>
2016-12-07 14:57:37 +0800

    debug dilate to run

--- dilate.m
+++ dilate.m
@@ -2,11 +2,12 @@ clear all;
 close all;
 clc;
 
-path_max = 'E:\指静脉与指背纹\Work\Code\NewestDatabase\max_cur\';
-path_max_dilate = 'E:\指静脉与指背纹\Work\Code\NewestDatabase\max_cur_dilate\';
+path_max = 'E:\指静脉与指背纹\Work\Code\NewestDatabase2\max_cur\';
+path_max_dilate = 'E:\指静脉与指背纹\Work\Code\NewestDatabase2\max_cur_dilate\';
 se = strel('disk', 7);
 
 for sampleID = 1:200
+    sampleID
     path1_max = [path_max, num2str(sampleID, '%.4d\\')];
     path1_max_dilate = [path_max_dilate, num2str(sampleID, '%.4d\\')];
     for dir = 1:3
@@ -14,7 +15,7 @@ for sampleID = 1:200
         path2_max_dilate = [path1_max_dilate, num2str(dir, '%d\\')];
         for id = 1:6
             path3_max = [path2_max, num2str(id, '%d.tiff')];
-            path3_max_dilate = [path2_max_dilate, num2str(id, '%d\\')];
+            path3_max_dilate = [path2_max_dilate, num2str(id, '%d.tiff')];
             
             img = imread(path3_max);
             img_dilate = imdilate(img, se);
